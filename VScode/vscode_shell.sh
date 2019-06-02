618dac2a63bf8a4edd1daee8684796333a9d77b8
William Mou <william.mou1024@gmail.com>
2019-06-02 14:13:19 +0800

    vscode vscode server

---
+++ VScode/vscode_shell.sh
@@ -0,0 +1,15 @@
+# apt isntall linux package
+apt update && apt install -y git python3 python3-pip tmux || echo "apt error !"
+
+# install python module
+pip3 install tensorflow keras opencv-python pandas matlab sklearn && echo "pip3 install ok" || echo "pip3 error"
+
+# isntall vscode server 
+wget https://github.com/cdr/code-server/releases/download/1.1119-vsc1.33.1/code-server1.1119-vsc1.33.1-linux-x64.tar.gz && tar xvf code-server1.1119-vsc1.33.1-linux-x64.tar.gz || echo "download error!"
+
+# download vscode plugin
+git clone https://github.com/William-Mou/ENV.git && mkdir -p /home/$(whoami)/ 
+cp -r /home/ENV/VScode/extensions /home/$(whoami)/
+
+# run coder-server
+code-server1.1119-vsc1.33.1-linux-x64/code-server ./$(whoami) --extensions-dir="./$(whoami)/extensions/" --password="root"  && echo "done" || echo "run server error"
